da6414c26d344319211f6ed911da5782d5b094a6
sitiafiyah <siti_afiyah_24rpl@student.smktelkom-mlg.sch.id>
2017-05-15 00:17:25 +0700

    DatabaseHelper

--- app/src/test/java/id/sch/smktelkom_mlg/privateassignment/xirpl533/projectpribadi/ExampleUnitTest.java
+++ app/src/test/java/id/sch/smktelkom_mlg/privateassignment/xirpl533/projectpribadi/ExampleUnitTest.java
@@ -1,8 +1,6 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl533.projectpribadi;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +8,8 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
 }
